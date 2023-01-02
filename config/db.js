f1dcdaa443e4c68a10f6e7bbb96d21ae102f3264
yyy1000 <992364620@qq.com>
2023-01-02 17:38:45 +0800

    user model, validation & user registration

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const db = config.get('mongoURI');
 const connectDB = async() => {
     try{
         mongoose.set('strictQuery', false);
-        await mongoose.connect(db,{ useNewUrlParser: true });
+        await mongoose.connect(db,{ 
+            useNewUrlParser: true ,
+            autoIndex: true
+        });
         
         console.log('MongoDB Connected...')
     } catch(err){
