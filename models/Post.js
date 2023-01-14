fc8a2092d50b39dbd2beb484858e8b82abaa7ff5
yyy1000 <992364620@qq.com>
2023-01-14 16:49:28 +0800

    feat: add Post model

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const moogoose = require('mongoose');
+const Schema = moogoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+});
+
+module.exports = Post = moogoose.model()
