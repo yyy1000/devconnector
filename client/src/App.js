671ca0f831770d49dbe8f5c6f9e7bef671a6395e
yyy1000 <992364620@qq.com>
2023-01-17 16:16:35 +0800

    feat: React Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import { Fragment } from 'react';
+import { React, Fragment } from 'react';
 import './App.css';
-import { Navbar } from './components/Navbar';
-import { Landing } from './components/Landing';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
+
 const App = () =>(
-  <Fragment>
-    <Navbar></Navbar>
-    <Landing></Landing>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
 );
 
 
