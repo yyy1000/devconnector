f847f439997aeeb41b467b5bafb887037a9851b6
yyy1000 <992364620@qq.com>
2023-01-23 21:43:23 +0800

    feat: create a redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,12 @@ import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
+// Redux
+import { Provider } from 'react';
+import store from './store';
 
 const App = () =>(
+  <Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
@@ -17,6 +21,7 @@ const App = () =>(
       </Routes>
     </Fragment>
   </Router>
+  </Provider>
 );
 
 
