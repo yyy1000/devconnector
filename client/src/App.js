7bdfb410ca4f9e02a9473f3eb5396e180b558bcc
yyy1000 <992364620@qq.com>
2023-01-25 18:03:43 +0800

    feat: Load user & auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { React, Fragment } from 'react';
+import { React, Fragment, useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
@@ -9,8 +9,18 @@ import { Login } from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthTOken';
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () =>{
+  useEffect( () => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
   <Provider store={store}>
     <Router>
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,8 +2,30 @@ import axios from 'axios';
 import {setAlert} from './alert'
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from './types';
+import setAuthToken from '../utils/setAuthTOken';
+// Load User
+export const loadUser = () => async dispatch => {
+    if(localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            tupe: USER_LOADED,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 // Register User
 
