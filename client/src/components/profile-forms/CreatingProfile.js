5cc63f43e9ff3cacda797d3527004ff9e2c82af7
yyy1000 <992364620@qq.com>
2023-01-29 17:12:55 +0800

    feat: CreateProfile action

--- client/src/components/profile-forms/CreatingProfile.js
+++ client/src/components/profile-forms/CreatingProfile.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import { Fragment } from 'react';
+import { createProfile } from '../../actions/profile';
+import { Link, useNavigate } from 'react-router-dom';
 
-const CreatingProfile = (props) => {
+const CreatingProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -18,6 +20,8 @@ const CreatingProfile = (props) => {
   });
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
+  
+  //const navigate = useNavigate();
 
   const {
     company,
@@ -31,8 +35,13 @@ const CreatingProfile = (props) => {
     youtube,
   } = formData;
 
-  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -41,7 +50,7 @@ const CreatingProfile = (props) => {
         profile stand out
       </p>
       <small>* = required fields</small>
-      <form className='form'>
+      <form className='form' onSubmit={onSubmit}>
         <div className='form-group'>
           <select name='status' value={status} onChange={(e) => onChange(e)}>
             <option value='0'>* Select Professional Status</option>
@@ -59,25 +68,49 @@ const CreatingProfile = (props) => {
           </small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='Company' name='company' value={company} onChange = {e => onChange(e)} />
+          <input
+            type='text'
+            placeholder='Company'
+            name='company'
+            value={company}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             Could be your own company or one you work for
           </small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='Website' name='website' value={website} onChange = {e => onChange(e)}/>
+          <input
+            type='text'
+            placeholder='Website'
+            name='website'
+            value={website}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             Could be your own or a company website
           </small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='Location' name='location' value={location} onChange = {e => onChange(e)}/>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             City & state suggested (eg. Boston, MA)
           </small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='* Skills' name='skills' value={skills} onChange = {e => onChange(e)} />
+          <input
+            type='text'
+            placeholder='* Skills'
+            name='skills'
+            value={skills}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
           </small>
@@ -87,7 +120,8 @@ const CreatingProfile = (props) => {
             type='text'
             placeholder='Github Username'
             name='githubusername'
-            value={githubusername} onChange = {e => onChange(e)}
+            value={githubusername}
+            onChange={(e) => onChange(e)}
           />
           <small className='form-text'>
             If you want your latest repos and a Github link, include your
@@ -95,7 +129,12 @@ const CreatingProfile = (props) => {
           </small>
         </div>
         <div className='form-group'>
-          <textarea placeholder='A short bio of yourself' name='bio' value={bio} onChange = {e => onChange(e)}></textarea>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={(e) => onChange(e)}
+          ></textarea>
           <small className='form-text'>Tell us a little about yourself</small>
         </div>
 
@@ -114,25 +153,38 @@ const CreatingProfile = (props) => {
           <Fragment>
             <div className='form-group social-input'>
               <i className='fab fa-youtube fa-2x'></i>
-              <input type='text' placeholder='YouTube URL' name='youtube' value={youtube} onChange = {e => onChange(e)}/>
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={(e) => onChange(e)}
+              />
             </div>
 
             <div className='form-group social-input'>
               <i className='fab fa-linkedin fa-2x'></i>
-              <input type='text' placeholder='Linkedin URL' name='linkedin' value={linkedin} onChange = {e => onChange(e)} />
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={(e) => onChange(e)}
+              />
             </div>
-
-            <input type='submit' className='btn btn-primary my-1' />
-            <a className='btn btn-light my-1' href='dashboard.html'>
-              Go Back
-            </a>
           </Fragment>
         )}
+        <input type='submit' className='btn btn-primary my-1' />
+        <a className='btn btn-light my-1' href='dashboard.html'>
+          Go Back
+        </a>
       </form>
     </Fragment>
   );
 };
 
-CreatingProfile.propTypes = {};
+CreatingProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreatingProfile;
+export default connect(null, { createProfile })(CreatingProfile);
