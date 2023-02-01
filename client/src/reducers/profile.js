0cc08b6541740ab8a0fab60611dfa5ba4cb106a1
yyy1000 <992364620@qq.com>
2023-02-01 16:59:30 +0800

    feat: finish profile actions & reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
 } from '../actions/types';
@@ -23,6 +25,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      }
     case PROFILE_ERROR:
       return {
         ...state,
@@ -36,6 +44,12 @@ export default function (state = initialState, action) {
         repos: [],
         loading: false,
       };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      }
     default:
       return state;
   }
