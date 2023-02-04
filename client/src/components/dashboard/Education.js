7099559799b823b0649370f5a7657138d8440553
yyy1000 <992364620@qq.com>
2023-02-04 19:38:13 +0800

    fix: auto  inproper function call

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -49,4 +49,4 @@ Education.propTypes = {
   deleteEducation: PropTypes.func.isRequired,
 };
 
-export default connect(null, deleteEducation)(Education);
+export default connect(null, { deleteEducation })(Education);
