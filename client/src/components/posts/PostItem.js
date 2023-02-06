e4d06910b9aa39ab5644be43ec4ffb64bdfb7beb
yyy1000 <992364620@qq.com>
2023-02-06 22:01:46 +0800

    fix: make code more clear

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -29,7 +29,7 @@ const PostItem = ({
         {showActions && (
           <Fragment>
             <button
-              onClick={(e) => addLike(_id)}
+              onClick={() => addLike(_id)}
               type='button'
               className='btn btn-light'
             >
@@ -37,7 +37,7 @@ const PostItem = ({
               <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
             </button>
             <button
-              onClick={(e) => removeLike(_id)}
+              onClick={() => removeLike(_id)}
               type='button'
               className='btn btn-light'
             >
@@ -51,7 +51,7 @@ const PostItem = ({
             </Link>
             {!auth.loading && user === auth.user._id && (
               <button
-                onClick={(e) => deletePost(_id)}
+                onClick={() => deletePost(_id)}
                 type='button'
                 className='btn btn-danger'
               >
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -107,10 +107,7 @@ router.put('/like/:id', auth, async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     // Check if the post has already been liked
-    if (
-      post.likes.filter((like) => like.user.toString() === req.user.id).length >
-      0
-    ) {
+    if (post.likes.some((like) => like.user.toString() === req.user.id)) {
       return res.status(400).json({ msg: 'Post already liked' });
     }
 
@@ -120,7 +117,6 @@ router.put('/like/:id', auth, async (req, res) => {
 
     return res.json(post.likes);
   } catch (err) {
-    console.error(err.message);
     if (err.kind === 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found' });
     }
